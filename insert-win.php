af4fedb21146fa7e33c4afd799df2d2bf4020df0
Ivo Ivanov <pifonsi@gmail.com>
2016-03-16 14:30:57 +0200

    ver 12
    first online version

---
+++ insert-win.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>SBB Ostern - Vielen Dank!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+	<div class="cards-wrapp wrapper">
+		<section id="cards">
+        <div id="thankyou">
+            <h2 style="opacity: 1; background: rgb(19, 165, 56);">Vielen Dank f&uuml;r Ihre Angaben, die Geschenkkarte Bahnhof wird Ihnen in den n&auml;chsten Wochen per Post zugestellt.</h2>
+        </div>
+        </section>
+    </div>
+</body>
+</html>';?>
--- js/game.js
+++ js/game.js
@@ -1,271 +1,287 @@
 jQuery(document).ready(function($) {
 
-	//Variables
-	var cardWidth = $('.card-wrapp').outerWidth();
-	$(window).resize(function() {
-		cardWidth = $('.card-wrapp').outerWidth();
-	});
+    //Variables
+    var cardWidth = $('.card-wrapp').outerWidth();
+    $(window).resize(function() {
+        cardWidth = $('.card-wrapp').outerWidth();
+    });
 
-	var left = cardWidth;
-	var top = 0;
-	var user, dealer;
-	var winner = true;
+    var left = cardWidth;
+    var top = 0;
+    var user, dealer, flash, winner;
 
-	//Check for mobile devices
-	function isTouchEnabled() {
-		try {
-			document.createEvent("TouchEvent");
-			return true;
-		} catch (e) {
-			return false;
-		}
-	}
+    function loadGame() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                winner = parseInt(xhttp.responseText);
+            }
+        };
+        xhttp.open("POST", "game-settings.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("");
+    }
 
-	//Identify mobile devices and small resolution screens
-	function isMobile() {
-		if ($(window).width() < 1000) {
-			$('body').addClass('mobile').removeClass('desktop');
-			return true;
-		} else {
-			$('body').addClass('desktop').removeClass('mobile');
-			return 0;
-		}
-	}
+    loadGame();
 
-	isMobile();
-	$(window).resize(isMobile);
+    //Check for mobile devices
+    function isTouchEnabled() {
+        try {
+            document.createEvent("TouchEvent");
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
 
+    //Identify mobile devices and small resolution screens
+    function isMobile() {
+        if ($(window).width() < 1000) {
+            $('body').addClass('mobile').removeClass('desktop');
+            return true;
+        } else {
+            $('body').addClass('desktop').removeClass('mobile');
+            return 0;
+        }
+    }
 
-	function constructor() {
-		var top = 0;
-		var left = 0;
-
-		$('.card-wrapp').each(function(index) {
-			var self = this;
-			$(self).css({
-				'display': 'block',
-				'transform': 'translate(' + left + 'px,' + top + 'px)'
-			});
-			left = left + 2;
-			top = top + 2;
-			setTimeout(function() {
-				$(self).css('opacity', 1);
-			}, index * 200);
-		});
-	}
-
-	constructor();
-
-	$('.card-wrapp').css('height', cardWidth + 'px');
-	$('#cards').css('height', (cardWidth + 16 + $('#cards').width()*0.06)| 0);
-	$(window).resize(function() {
-		$('.card-wrapp').css('height', cardWidth + 'px');
-		$('#cards').css('height', (cardWidth*3 + $('#cards').width()*0.06)| 0);
-	});
+    isMobile();
+    $(window).resize(isMobile);
 
 
-	$('body').on('click', '.gamebutton.notclicked, #cards.notclicked .card', function() {
-		mix();
+    function constructor() {
+        var top = 0;
+        var left = 0;
 
-		setTimeout(function() {
-			$('.notclicked').removeClass('notclicked');
-			$('.card').addClass('notclicked userturn');
-			$('.gamebutton').css({
-				'opacity': 0.5,
-				'cursor': 'default'
-			});
-		}, 1000);
-	});
+        $('.card-wrapp').each(function(index) {
+            var self = this;
+            $(self).css({
+                'display': 'block',
+                '-webkit-transform': 'translate(' + left + 'px,' + top + 'px)',
+                'transform': 'translate(' + left + 'px,' + top + 'px)'
+            });
+            left = left + 2;
+            top = top + 2;
+            setTimeout(function() {
+                $(self).css('opacity', 1);
+            }, index * 200);
+        });
+    }
 
-	function mix() {
-		$('#cards').css('height', (cardWidth*3 + $('#cards').width()*0.06)| 0);
-		var count = 2;
-		$($('.card-wrapp').get().reverse()).each(function(index) {
-			var self = this;
+    constructor();
 
-			setTimeout(function() {
-
-				if ((count % 3 === 0) && (count < 10)) {
-					$(self).css({
-						'transform': 'translate(' + left + 'px,' + top + 'px)',
-						'transition': 'transform 1s',
-						'z-index': count + 10
-					});
-					left = 0;
-					top += cardWidth;
-
-				} else if (count < 10) {
-					$(self).css({
-						'transform': 'translate(' + left + 'px,' + top + 'px)',
-						'transition': 'transform 1s',
-						'z-index': count + 10
-					});
-					left += cardWidth;
-				}
-				count++;
+    $('.card-wrapp').css('height', cardWidth + 'px');
+    $('#cards').css('height', (cardWidth + 16 + $('#cards').width() * 0.06) | 0);
+    $(window).resize(function() {
+        $('.card-wrapp').css('height', cardWidth + 'px');
+        $('#cards').css('height', (cardWidth * 3 + $('#cards').width() * 0.06) | 0);
+    });
 
 
-			}, index * 450);
-		});
-	}
+    $('body').on('click', '.gamebutton.notclicked, #cards.notclicked .card', function() {
+        mix();
 
-	function userturn() {
-		$(document).on('click', '.userturn.notclicked', function() {
-console.log(this);
-			$('.userturn').removeClass('notclicked userturn');
-			$('.card').addClass('dealerturn');
-			$(this).parent().addClass('user');
-			$(this).find('.card-back').addClass(user);
-			$(this).addClass('flipped');
-		});
-	}
+        setTimeout(function() {
+            $('.notclicked').removeClass('notclicked');
+            $('.card').addClass('notclicked userturn');
+            $('.gamebutton').css({
+                'opacity': 0.5,
+                'cursor': 'default'
+            });
+        }, 1000);
 
-	function dealerturn() {
-		$(document).on('click', '.card-wrapp:not(.user) .dealerturn', function() {
-			$(this).parent().addClass('dealer');
-			$(this).find('.card-back').addClass(dealer);
+        game(winner);
+    });
 
-			$('.card-wrapp:not(.user):not(.dealer) .card').css({
-				'transform': 'rotate(360deg)',
-				'transition': 'transform 2s'
-			}).fadeOut(1300, function() {
-				$('.card-wrapp:not(.user):not(.dealer)').css('display', 'none');
-			});
+    function mix() {
 
-			$('.card-wrapp:not(.user):not(.dealer)').css('transform', 'translate(' + cardWidth + 'px,' + cardWidth * 2 + 'px)').css('transition', 'transform 3s');
+        $('#cards').css('height', (cardWidth * 3 + $('#cards').width() * 0.06) | 0);
+        var count = 2;
+        $($('.card-wrapp').get().reverse()).each(function(index) {
+            var self = this;
 
-			flop();
-			$(this).addClass('flipped');
-			msg();
+            setTimeout(function() {
 
+                if ((count % 3 === 0) && (count < 10)) {
+                    $(self).css({
+                        '-webkit-transform': 'translate(' + left + 'px,' + top + 'px)',
+                        'transform': 'translate(' + left + 'px,' + top + 'px)',
+                        'transition': 'transform 1s',
+                        'z-index': count + 10
+                    });
+                    left = 0;
+                    top += cardWidth;
 
+                } else if (count < 10) {
+                    $(self).css({
+                        '-webkit-transform': 'translate(' + left + 'px,' + top + 'px)',
+                        'transform': 'translate(' + left + 'px,' + top + 'px)',
+                        'transition': 'transform 1s',
+                        'z-index': count + 10
+                    });
+                    left += cardWidth;
+                }
 
-		});
-	}
-
-	userturn();
-	dealerturn();
-
-
-	function flop() {
-
-		$('.user').css({
-			'transform': 'translate(' + cardWidth / 2 + 'px,0)',
-			'transition': 'transform 2s'
-		});
-		$('.dealer').css({
-			'transform': 'translate(' + (cardWidth / 2 + cardWidth) + 'px,0)',
-			'transition': 'transform 2s'
-		});
-
-	}
+                count++;
 
 
-	function game(result) {
-		var randorder = shuffle(['carrot', 'bunny', 'egg']);
-		var removeItem;
+            }, index * 350);
+        });
+    }
 
-		if (result) {
-			removeItem = 'egg';
-			randorder = jQuery.grep(randorder, function(value) {
-				return value != removeItem;
-			});
-			var index = 0;
+    function userturn() {
+        $(document).on('click', '.userturn.notclicked', function() {
+            $('.userturn').removeClass('notclicked userturn');
+            $('.card').addClass('dealerturn');
+            $(this).parent().addClass('user');
+            $(this).find('.card-back').addClass(user);
+            $(this).addClass('flipped');
+        });
+    }
 
-			console.log('win!');
+    function dealerturn() {
+        $(document).on('click', '.card-wrapp:not(.user) .dealerturn', function() {
+            $(this).parent().addClass('dealer');
+            $(this).find('.card-back').addClass(dealer);
 
-			user = randorder[index++];
-			dealer = randorder[index++];
-		} else {
+            $('.card-wrapp:not(.user):not(.dealer) .card').css({
+                '-webkit-transform': 'rotate(360deg)',
+                'transform': 'rotate(360deg)',
+                'transition': 'transform 2s'
+            }).fadeOut(1300, function() {
+                $('.card-wrapp:not(.user):not(.dealer)').css('display', 'none');
+            });
 
-			var ind = 0;
+            $('.card-wrapp:not(.user):not(.dealer)').css({
+                '-webkit-transform': 'translate(' + cardWidth + 'px,' + cardWidth * 2 + 'px)',
+                'transform': 'translate(' + cardWidth + 'px,' + cardWidth * 2 + 'px)',
+                'transition': 'transform 3s'
+            });
 
-			console.log('lose!');
-			user = randorder[ind++];
-			flash = user;
-
-			if (flash == 'carrot') {
-				removeItem = 'bunny';
-				randorder = jQuery.grep(randorder, function(value) {
-					return value != removeItem;
-				});
-			} else if (flash == 'bunny') {
-				removeItem = 'carrot';
-				randorder = jQuery.grep(randorder, function(value) {
-					return value != removeItem;
-				});
-			}
-
-			dealer = randorder[(Math.floor(Math.random() * randorder.length) + 1) - 1];
-
-		}
-
-	}
-
-	function shuffle(o) {
-		for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-		return o;
-	}
-
-	game(winner);
-	console.log(user, dealer);
-
-	function msg() {
-		if (winner) {
-			$('#result').css({
-				'margin-top' : cardWidth + 80,
-				'display' : 'block'
-			});
-
-			$('#result h2').css({
-				'background' : '#13a538',
-				'opacity' : 1
-			}).text('Sie haben gewonnen!');
-
-
-		} else {
-			$('#result').css({
-				'margin-top' : cardWidth + 80,
-				'display' : 'block'
-			});
-
-			$('#result h2').css({
-				'background' : '#d10a11',
-				'opacity' : 1
-			}).text('Sie haben leider verloren!');
-		}
-		setTimeout(function() {
-			formConstructor();
-		}, 1000);
-	}
-
-	function formConstructor(){
-		if(winner){
-			$('#gameform').css({
-				'display' : 'block',
-				'opacity' : 1
-			});
-
-			$('#cards').css('height', 'auto');
-
-		}
-		else{
-			$('#gameform > label:nth-child(9),#gameform > input[type="text"]:nth-child(10),#gameform > label:nth-child(11),#gameform > input[type="text"]:nth-child(12)').remove();
-			$('#gameform').css({
-				'display' : 'block',
-				'opacity' : 1
-			});
-
-			$('#cards').css('height', 'auto');
-		}
-	}
+            flop();
+            $(this).addClass('flipped');
+            msg();
 
 
 
+        });
+    }
+
+    userturn();
+    dealerturn();
 
 
+    function flop() {
+
+        $('.user').css({
+            '-webkit-transform': 'translate(' + cardWidth / 2 + 'px,0)',
+            'transform': 'translate(' + cardWidth / 2 + 'px,0)',
+            'transition': 'transform 2s'
+        });
+        $('.dealer').css({
+            '-webkit-transform': 'translate(' + (cardWidth / 2 + cardWidth) + 'px,0)',
+            'transform': 'translate(' + (cardWidth / 2 + cardWidth) + 'px,0)',
+            'transition': 'transform 2s'
+        });
+
+    }
 
 
+    function game(result) {
+        var randorder = shuffle(['carrot', 'bunny', 'egg']);
+        var removeItem;
+
+        if (result) {
+            removeItem = 'egg';
+            randorder = jQuery.grep(randorder, function(value) {
+                return value != removeItem;
+            });
+            var index = 0;
+
+            user = randorder[index++];
+            dealer = randorder[index++];
+        } else {
+
+            var ind = 0;
+
+            user = randorder[ind++];
+            flash = user;
+
+            if (flash == 'carrot') {
+                removeItem = 'bunny';
+                randorder = jQuery.grep(randorder, function(value) {
+                    return value != removeItem;
+                });
+            } else if (flash == 'bunny') {
+                removeItem = 'carrot';
+                randorder = jQuery.grep(randorder, function(value) {
+                    return value != removeItem;
+                });
+            }
+
+            dealer = randorder[(Math.floor(Math.random() * randorder.length) + 1) - 1];
+
+        }
+
+    }
+
+    function shuffle(o) {
+        for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+
+    function msg() {
+        if (winner) {
+            $('#gameform').css({
+                'display': 'block',
+                'opacity': 1
+            });
+
+            $('#result').css({
+                'margin-top': cardWidth + 80,
+                'display': 'block'
+            });
+
+            $('#result h2').css({
+                'background': '#13a538',
+                'opacity': 1
+            }).text('Sie haben gewonnen!');
+
+            $('#gameform').attr('action', 'insert-win.php');
+
+        } else {
+            $('#gameform > label:nth-child(7),#gameform > input[type="text"]:nth-child(8),#gameform > label:nth-child(9),#gameform > input[type="text"]:nth-child(10)').remove();
+
+            $('#gameform').css({
+                'display': 'block',
+                'opacity': 1
+            });
+
+            $('#result').css({
+                'margin-top': cardWidth + 80,
+                'display': 'block'
+            });
+
+            $('#result h2').css({
+                'background': '#d10a11',
+                'opacity': 1
+            }).text('Sie haben leider verloren!');
+
+            $('#gameform').attr('action', 'insert-lose.php');
+        }
+
+        $('#cards').css('height', 'auto');
+
+    }
+
+
+    $('input[name="newsletter"]').on('click', function() {
+        if ($(this).prop('checked')) {
+            $('#bahnhof').slideToggle();
+        } else {
+            $('#bahnhof').slideToggle();
+        }
+    });
 
 
 
